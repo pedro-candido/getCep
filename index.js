878a3042ec743125713282dc9f1bdbdff0212a71
Pedro Candido <pedroferreirac@hotmail.com>
2024-01-16 11:24:34 -0300

    refactor: replacing module files

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getAddress = void 0;
+var getCep_1 = require("./src/getCep");
+Object.defineProperty(exports, "getAddress", { enumerable: true, get: function () { return getCep_1.getAddress; } });
